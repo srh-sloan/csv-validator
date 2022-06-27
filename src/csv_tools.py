23121122939b30e224cc9dcd1461f74c39ed0da1
Sarah Sloan <srh.sloan@gmail.com>
2022-06-27 20:58:11 +0100

    validates a single csv

--- src/csv_tools.py
+++ src/csv_tools.py
@@ -3,19 +3,32 @@ import os
 
 
 def validate_csv_file(expected_cols, expected_digits, csv_path):
-    print(os.path.dirname(os.path.abspath(__file__)))
     print('validating csv at', csv_path)
+    valid = True
+    
     with open(csv_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         row_count = 0;
-        valid = True
         for row in csv_reader:
-            print('\t',row)
+            #print('\t',row)
             row_count+=1
-            if len(row) > 1:
-                print('\tRow',row_count,'has >',expected_cols,'columns:',row)
+            if len(row) != expected_cols:
+                print('\tRow',row_count,'has',len(row),'columns:',row,'(expected: ',expected_cols,')')
                 valid = False
-                break
+            if valid == True:
+                col_count = 0
+                for col in row:
+                    col_count += 1
+                    if len(col) != expected_digits:
+                        print('\tRow',row_count,'column',col_count, 'has ',len(col),'digits:',col,'(expected: ',expected_digits,')')
+                        valid = False
+                    if not col.isnumeric():
+                        print('\tRow',row_count,'column',col_count, 'contains non-numeric characters:',row)
+                        valid = False
+    if row_count == 0:
+        valid = False
+        print('\tInvalid: no rows')
+    print('CSV at',csv_path,'was valid:',valid,'\n')
     return valid
 
 
--- tests/test_validate.py
+++ tests/test_validate.py
@@ -2,20 +2,38 @@ import csv_tools as tools
 import os
 
 def test_csv_valid():
-    print(os.path.dirname(os.path.abspath(__file__)))
     csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/test-data/3/1/5.csv'
     
     assert tools.validate_csv_file(1,12, csv_path) == True
 
 def test_csv_invalid_columns_all():
-    print(os.path.dirname(os.path.abspath(__file__)))
     csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/multi-column.csv'
     
     assert tools.validate_csv_file(1,12, csv_path) == False
 
 
 def test_csv_invalid_columns_some():
-    print(os.path.dirname(os.path.abspath(__file__)))
     csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/some-multi-column.csv'
+    
+    assert tools.validate_csv_file(1,12, csv_path) == False
+
+def test_csv_invalid_empty_sheet():
+    csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/empty.csv'
+    
+    assert tools.validate_csv_file(1,12, csv_path) == False
+
+
+def test_csv_invalid_empty_row():
+    csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/empty-field.csv'
+    
+    assert tools.validate_csv_file(1,12, csv_path) == False
+
+def test_csv_invalid_bad_ids_length():
+    csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/invalid-length.csv'
+    
+    assert tools.validate_csv_file(1,12, csv_path) == False
+
+def test_csv_invalid_bad_ids_chars():
+    csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/invalid-chars.csv'
     
     assert tools.validate_csv_file(1,12, csv_path) == False
