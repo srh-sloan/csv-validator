851b91f85034a494b20b99591aeefda884312217
Sarah Sloan <srh.sloan@gmail.com>
2022-06-28 09:42:20 +0100

    restructured to gather output for multiple files

--- src/csv_tools.py
+++ src/csv_tools.py
@@ -1,43 +1,66 @@
 import csv
 
-
-def validate_csv_file(expected_cols, expected_digits, csv_path):
-    print('validating csv at', csv_path)
-    valid = True
-    
-    with open(csv_path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        row_count = 0;
-        for row in csv_reader:
-            #print('\t',row)
-            row_count+=1
-
-            # Check if we have the expectd number of columns in this row
-            if len(row) != expected_cols:
-                print('\tRow',row_count,'has',len(row),'columns:',row,'(expected: ',expected_cols,')')
-                valid = False
-
-            # Check format
-            if valid == True:
-                col_count = 0
-                for col in row:
-                    col_count += 1
-
-                    # Check for number of digits
-                    if len(col) != expected_digits:
-                        print('\tRow',row_count,'column',col_count, 'has ',len(col),'digits:',col,'(expected: ',expected_digits,')')
-                        valid = False
-
-                    # Check is numeric    
-                    if not col.isnumeric():
-                        print('\tRow',row_count,'column',col_count, 'contains non-numeric characters:',row)
-                        valid = False
-
-    # If row_count is 0, it was empty
-    if row_count == 0:
-        valid = False
-        print('\tInvalid: no rows')
-    print('CSV at',csv_path,'was valid:',valid,'\n')
-    return valid
+class Csv_Validator:
 
 
+    def __init__(self, expected_cols, expected_digits):
+        self.errors = []
+        self.output = []
+        self.total_rows = 0
+        self.expected_cols = expected_cols
+        self.expected_digits = expected_digits
+        
+
+    def validate_csv_file(self, csv_path):
+        self.errors.append(f"validating csv at {csv_path}")
+        self.output.append(f"validating csv at {csv_path}")
+        valid = True
+        
+        with open(csv_path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            row_count = 0;
+            for row in csv_reader:
+                #print('\t',row)
+                row_count+=1
+
+                # Check if we have the expectd number of columns in this row
+                if len(row) != self.expected_cols:
+                    self.errors.append(f"\tRow {row_count} has {len(row)} columns:{row} (expected: {self.expected_cols})")
+                    valid = False
+
+                # Check format
+                if valid == True:
+                    col_count = 0
+                    for col in row:
+                        col_count += 1
+
+                        # Check for number of digits
+                        if len(col) != self.expected_digits:
+                            self.errors.append(f"\tRow {row_count} column {col_count} has {len(col)} digits:' {col} (expected: {self.expected_digits})")
+                            valid = False
+
+                        # Check is numeric    
+                        if not col.isnumeric():
+                            self.errors.append(f"\tRow {row_count} column {col_count} contains non-numeric characters: {row}")
+                            valid = False
+
+        # If row_count is 0, it was empty
+        if row_count == 0:
+            valid = False
+            self.errors.append(f"\tInvalid: no rows")
+        self.output.append(f"CSV at {csv_path} was valid: {valid} \n")
+        self.total_rows += row_count
+        return valid
+
+    def print_results(self):
+        print('\nERRORS\n')
+        print(self.errors)
+        print('\n\nOutput\n')
+        print(self.output)
+        print('\n\nTotal Rows:',self.total_rows)
+
+
+    def validate_in_folder(self, csv_path):
+        valid = self.validate_csv_file(csv_path)
+        self.print_results
+        return valid
--- tests/test_validate_file.py
+++ tests/test_validate_file.py
@@ -1,39 +1,45 @@
-import csv_tools as tools
+from csv_tools import Csv_Validator
 import os
 
 def test_csv_valid():
     csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/test-data/3/1/5.csv'
-    
-    assert tools.validate_csv_file(1,12, csv_path) == True
+    tools = Csv_Validator(1,12)
+    assert tools.validate_csv_file(csv_path) == True
 
 def test_csv_invalid_columns_all():
     csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/multi-column.csv'
+    tools = Csv_Validator(1,12)
     
-    assert tools.validate_csv_file(1,12, csv_path) == False
+    assert tools.validate_csv_file(csv_path) == False
 
 
 def test_csv_invalid_columns_some():
     csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/some-multi-column.csv'
+    tools = Csv_Validator(1,12)
     
-    assert tools.validate_csv_file(1,12, csv_path) == False
+    assert tools.validate_csv_file(csv_path) == False
 
 def test_csv_invalid_empty_sheet():
     csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/empty.csv'
+    tools = Csv_Validator(1,12)
     
-    assert tools.validate_csv_file(1,12, csv_path) == False
+    assert tools.validate_csv_file(csv_path) == False
 
 
 def test_csv_invalid_empty_row():
     csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/empty-field.csv'
+    tools = Csv_Validator(1,12)
     
-    assert tools.validate_csv_file(1,12, csv_path) == False
+    assert tools.validate_csv_file(csv_path) == False
 
 def test_csv_invalid_bad_ids_length():
     csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/invalid-length.csv'
+    tools = Csv_Validator(1,12)
     
-    assert tools.validate_csv_file(1,12, csv_path) == False
+    assert tools.validate_csv_file(csv_path) == False
 
 def test_csv_invalid_bad_ids_chars():
     csv_path = '/home/srhsloan/test-workspace/csv-validator/tests/invalid-data/invalid-chars.csv'
+    tools = Csv_Validator(1,12)
     
-    assert tools.validate_csv_file(1,12, csv_path) == False
+    assert tools.validate_csv_file(csv_path) == False
